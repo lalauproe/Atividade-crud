e87b5c81a0385ba26d3226e8222da67e1be6005f
lalauproe <laurinha.castro.neves.proenca@gmail.com>
2025-08-20 12:22:15 -0300

    criando conexao/salvar vinculando o projeto com banco de dados mysql

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP CRUD MYSQL</title>
-
+    <!-- BOOTSTRAP 4 -->
     <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
-    <!-- FONT AWESOEM -->
+    <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
         crossorigin="anonymous">
@@ -23,26 +23,24 @@
         <main class="container p-4">
             <div class="row">
                 <div class="col-md-4">
-
+                    <!-- Formulário -->
                     <div class="card card-body">
-                        <form action="#" method="POST">
+                        <form action="save.php" method="POST">
                             <div class="form-group">
-                                <input type="text" name="titulo" class="form-control"
-                                    placeholder="Task Title" autofocus>
+                                <input type="text" name="title" class="form-control"
+                                    placeholder="Titulo da tarefa" autofocus>
                             </div>
                             <div class="form-group">
-                                <textarea name="descricao" rows="2" class="from-control"
-                                    placeholder="descricao da tarefa">
-                                </textarea>
+                                <textarea name="description" rows="2" class="form-control" placeholder="Descrição"></textarea>
                             </div>
                             <input type="submit" name="salvar" class="btn btn-success btn-block"
-                                value="Save Task">
+                                value="Salvar Tarefa">
                         </form>
                     </div>
                 </div>
+                <!-- /Formulário -->
 
-
-
+                <!-- Tabela de Tarefas -->
                 <div class="col-md-8">
                     <table class="table table-bordered">
                         <thead>
@@ -71,12 +69,12 @@
                         </tbody>
                     </table>
                 </div>
-
+                <!-- /Tabela de Tarefas -->
             </div>
         </main>
     </div>
 
-
+    <!-- BOOTSTRAP 4 SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>

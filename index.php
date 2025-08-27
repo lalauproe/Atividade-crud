5b7e75a4775e35686b8000ef0662e902cd41dfe8
lalauproe <laurinha.castro.neves.proenca@gmail.com>
2025-08-27 15:23:15 -0300

    criando a funcionalidade edit.php e fazendo funcionar o CRUD completo

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                                 <td><?php echo $row ['title']; ?></td>
                                 <td><?php echo $row ['description']; ?></td>
                                 <td><?php echo $row ['created_at']; ?></td>
-                                <td><a href="#" class="btn btn-secondary">
+                                <td><a href="edit.php?id=<?=$row['id']; ?>" class="btn btn-secondary">
                                         <i class="fas fa-marker"></i>
                                     </a>
                                     <a href="delete.php?id=<?=$row['id']; ?>" class="btn btn-danger">

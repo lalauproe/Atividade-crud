8afbeac146da18ed2214f6c961c797385e2cadb8
lalauproe <laurinha.castro.neves.proenca@gmail.com>
2025-08-28 10:56:40 -0300

    criando a tela de login, criando botão de logout com icone

--- index.php
+++ index.php
@@ -18,6 +18,11 @@
         <nav class="navbar navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="index.php">Crud PHP</a>
+                <form action="login.php" method="GET" class="d-inline">
+                    <button type="submit" class="btn btn-secondary">
+                        <i class="fas fa-sign-out-alt"></i>Logout
+                    </button>
+                </form>
             </div>
         </nav>
         <main class="container p-4">
@@ -67,7 +72,7 @@
                                 <td><?php echo $row['id']; ?></td>
                                 <td><?php echo $row ['title']; ?></td>
                                 <td><?php echo $row ['description']; ?></td>
-                                <td><?php echo $row ['created_at']; ?></td>
+                                <td><?=date("d/m/Y H:i:s", strtotime($row ['created_at'])); ?></td>
                                 <td><a href="edit.php?id=<?=$row['id']; ?>" class="btn btn-secondary">
                                         <i class="fas fa-marker"></i>
                                     </a>
